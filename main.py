59294e57e71ca46e9944bf46cf6e84951a614fe4
Brianna <81735269+brmu22@users.noreply.github.com>
2025-01-14 15:17:18 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from exa_py import Exa
+exa = Exa(api_key='YOUR_EXA_API_KEY') 
+
+# Take user's query input
+query = input('Search here: ')
+
+# Perform search 
+response = exa.search(
+    query,
+    num_results=10,
+    include_domains=["https://www.tiktok.com", "https://www.mynetdiary.com/"],
+)
+
+# Print results
+for result in response.results:
+    print(f'Title: {result.title}')
+    print(f'URL: {result.url}')
+    print()
